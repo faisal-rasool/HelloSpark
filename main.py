5c740254031875c9d61e288d1e0df1ef373b77cd
Faisal <faisal.rasool@hotmail.com>
2023-09-23 00:26:25 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    from pyspark import SparkContext, SparkConf
+
+    conf = SparkConf().setAppName('MyFirstStandaloneApp')
+    sc = SparkContext(conf=conf)
+
+    text_file = sc.textFile("./shakespeare.txt")
+
+    counts = text_file.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
+
+    print("Number of elements: " + str(counts.count()))
+    counts.saveAsTextFile("./shakespeareWordCount")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
